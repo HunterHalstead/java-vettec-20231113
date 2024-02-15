944c78d9372b20e51ef907cd39c64ecf0f1c55be
ericahicks <48265629+ericahicks@users.noreply.github.com>
2024-02-15 11:46:49 -0500

    jacoco config added

--- week13/day4/junit-lifecycle-practice/library-app/src/test/java/com/skillstorm/BookDaoTests.java
+++ week13/day4/junit-lifecycle-practice/library-app/src/test/java/com/skillstorm/BookDaoTests.java
@@ -45,44 +45,44 @@ public class BookDaoTests {
 		}
 	}
 	
-	@BeforeEach
-	void addData() {
-		try (Connection conn = config.getConnection()) {
-			int authorId = getId("author", "Jane Austen", conn);
-			int genreId = getId("genre", "Fiction", conn);
-			List<Book> books = new ArrayList<>();
-			books.add(new Book(0, "Pride and Prejudice", authorId, 1813, "9780141199078", genreId));
-			books.add(new Book(0, "Emma", authorId, 1815, "9781551113210", genreId));
-			String sql = "INSERT INTO testlibrary.book (title, author_id, publication_year, isbn, genre_id) VALUES "
-					+ "(?, ?, ?, ?, ?);";
-			PreparedStatement stmt = conn.prepareStatement(sql);
-			// Reuse the preparedStatement in the loop
-			int  counter = 0;
-			for (Book book : books) {
-				
-				stmt.setString(1, book.getTitle());
-				stmt.setInt(2, book.getAuthorId());
-				stmt.setInt(3, book.getPublicationYear());
-				stmt.setString(4, book.getISBN());
-				stmt.setInt(5, book.getGenreId());
-				// Execute statement
-				counter += stmt.executeUpdate();
-			}
-			System.out.println("Inserted " + counter + " rows in Book");
-			
-		} catch (SQLException e) {
-			System.out.println("Unable to get connection: " + e.getMessage());
-		}
-	}
-	
-	private int getId(String table, String value, Connection conn) throws SQLException {
-		String sql = "SELECT " + table + "_id FROM testlibrary." + table + " WHERE " + table + "_name = ?;";
-		PreparedStatement stmt = conn.prepareStatement(sql);
-		ResultSet rs = stmt.executeQuery();
-		if (rs.next())
-			return rs.getInt(1);
-		throw new SQLException("No " + "_id found for " + table + "_name of " + value);
-	}
+//	@BeforeEach
+//	void addData() {
+//		try (Connection conn = config.getConnection()) {
+//			int authorId = getId("author", "Jane Austen", conn);
+//			int genreId = getId("genre", "Fiction", conn);
+//			List<Book> books = new ArrayList<>();
+//			books.add(new Book(0, "Pride and Prejudice", authorId, 1813, "9780141199078", genreId));
+//			books.add(new Book(0, "Emma", authorId, 1815, "9781551113210", genreId));
+//			String sql = "INSERT INTO testlibrary.book (title, author_id, publication_year, isbn, genre_id) VALUES "
+//					+ "(?, ?, ?, ?, ?);";
+//			PreparedStatement stmt = conn.prepareStatement(sql);
+//			// Reuse the preparedStatement in the loop
+//			int  counter = 0;
+//			for (Book book : books) {
+//				
+//				stmt.setString(1, book.getTitle());
+//				stmt.setInt(2, book.getAuthorId());
+//				stmt.setInt(3, book.getPublicationYear());
+//				stmt.setString(4, book.getISBN());
+//				stmt.setInt(5, book.getGenreId());
+//				// Execute statement
+//				counter += stmt.executeUpdate();
+//			}
+//			System.out.println("Inserted " + counter + " rows in Book");
+//			
+//		} catch (SQLException e) {
+//			System.out.println("Unable to get connection: " + e.getMessage());
+//		}
+//	}
+//	
+//	private int getId(String table, String value, Connection conn) throws SQLException {
+//		String sql = "SELECT " + table + "_id FROM testlibrary." + table + " WHERE " + table + "_name = ?;";
+//		PreparedStatement stmt = conn.prepareStatement(sql);
+//		ResultSet rs = stmt.executeQuery();
+//		if (rs.next())
+//			return rs.getInt(1);
+//		throw new SQLException("No " + "_id found for " + table + "_name of " + value);
+//	}
 
 	@Test
 	void test1() {
